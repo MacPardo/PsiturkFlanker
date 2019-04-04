18db3d01543723853e930de41f34b8b89aa403e0
marcos <marcos.adamczuk@gmail.com>
2019-04-04 03:54:40 -0300

    added oci-r form

--- static/js/task.js
+++ static/js/task.js
@@ -29,7 +29,8 @@ var pages = [
   "flankerInstructions/instruct-1.html",
   "stage.html",
   "flankerStage.html",
-  "postquestionnaire.html"
+  "postquestionnaire.html",
+  "exp/OCI-R.html"
 ];
 
 psiTurk.preloadPages(pages);
@@ -56,6 +57,10 @@ var flankerInstructionPages = [
  *
  ********************/
 
+/**********************
+ * BEGIN FLANKER CODE *
+ **********************/
+ 
 /**
  * 
  * @param {Number} nTrials 
@@ -246,174 +251,20 @@ var FlankerExperiment = function(isPractice) {
   
   window.addEventListener('keydown', startFlanker);
 };
-
 /********************
- * STROOP TEST       *
+ * END FLANKER CODE *
  ********************/
-var StroopExperiment = function() {
-  var wordon, // time word is presented
-    listening = false;
 
-  // Stimuli for a basic Stroop experiment
-  var stims = [
-    ["SHIP", "red", "unrelated"],
-    ["MONKEY", "green", "unrelated"],
-    ["ZAMBONI", "blue", "unrelated"],
-    ["RED", "red", "congruent"],
-    ["GREEN", "green", "congruent"],
-    ["BLUE", "blue", "congruent"],
-    ["GREEN", "red", "incongruent"],
-    ["BLUE", "green", "incongruent"],
-    ["RED", "blue", "incongruent"]
-  ];
+/**
+ * BEGIN OCI-R form code
+ */
+var OciRQuestionnare = function() {
+  psiTurk.showPage("exp/OCI-R.html");
+}
+/**
+ * END OCI-R form code
+ */
 
-  stims = _.shuffle(stims);
-
-  var next = function() {
-    if (stims.length === 0) {
-      finish();
-    } else {
-      stim = stims.shift();
-      show_word(stim[0], stim[1]);
-      wordon = new Date().getTime();
-      listening = true;
-      d3.select("#query").html(
-        '<p id="prompt">Type "R" for Red, "B" for blue, "G" for green.</p>'
-      );
-    }
-  };
-
-  var response_handler = function(e) {
-    if (!listening) return;
-
-    var keyCode = e.keyCode,
-      response;
-
-    switch (keyCode) {
-      case 82:
-        // "R"
-        response = "red";
-        break;
-      case 71:
-        // "G"
-        response = "green";
-        break;
-      case 66:
-        // "B"
-        response = "blue";
-        break;
-      default:
-        response = "";
-        break;
-    }
-    if (response.length > 0) {
-      listening = false;
-      var hit = response == stim[1];
-      var rt = new Date().getTime() - wordon;
-
-      psiTurk.recordTrialData({
-        phase: "TEST",
-        word: stim[0],
-        color: stim[1],
-        relation: stim[2],
-        response: response,
-        hit: hit,
-        rt: rt
-      });
-      remove_word();
-      next();
-    }
-  };
-
-  var finish = function() {
-    $("body").unbind("keydown", response_handler); // Unbind keys
-    currentview = new Questionnaire();
-  };
-
-  var show_word = function(text, color) {
-    d3.select("#stim")
-      .append("div")
-      .attr("id", "word")
-      .style("color", color)
-      .style("text-align", "center")
-      .style("font-size", "150px")
-      .style("font-weight", "400")
-      .style("margin", "20px")
-      .text(text);
-  };
-
-  var remove_word = function() {
-    d3.select("#word").remove();
-  };
-
-  // Load the stage.html snippet into the body of the page
-  psiTurk.showPage("stage.html");
-
-  // Register the response handler that is defined above to handle any
-  // key down events.
-  $("body")
-    .focus()
-    .keydown(response_handler);
-
-  // Start the test
-  next();
-};
-
-/****************
- * Questionnaire *
- ****************/
-
-var Questionnaire = function() {
-  var error_message =
-    "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
-
-  record_responses = function() {
-    psiTurk.recordTrialData({ phase: "postquestionnaire", status: "submit" });
-
-    $("textarea").each(function(i, val) {
-      psiTurk.recordUnstructuredData(this.id, this.value);
-    });
-    $("select").each(function(i, val) {
-      psiTurk.recordUnstructuredData(this.id, this.value);
-    });
-  };
-
-  prompt_resubmit = function() {
-    document.body.innerHTML = error_message;
-    $("#resubmit").click(resubmit);
-  };
-
-  resubmit = function() {
-    document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-    reprompt = setTimeout(prompt_resubmit, 10000);
-
-    psiTurk.saveData({
-      success: function() {
-        clearInterval(reprompt);
-        psiTurk.computeBonus("compute_bonus", function() {
-          psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-        });
-      },
-      error: prompt_resubmit
-    });
-  };
-
-  // Load the questionnaire snippet
-  psiTurk.showPage("postquestionnaire.html");
-  psiTurk.recordTrialData({ phase: "postquestionnaire", status: "begin" });
-
-  $("#next").click(function() {
-    record_responses();
-    psiTurk.saveData({
-      success: function() {
-        psiTurk.computeBonus("compute_bonus", function() {
-          psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-        });
-      },
-      error: prompt_resubmit
-    });
-  });
-};
 
 // Task object to keep track of the current phase
 var currentview;
@@ -422,11 +273,12 @@ var currentview;
  * Run Task
  ******************/
 $(window).load(function() {
-  psiTurk.doInstructions(
-    flankerInstructionPages, // a list of pages you want to display in sequence
-    function() {
-      // currentview = new StroopExperiment();
-      currentview = new FlankerExperiment();
-    } // what you want to do when you are done with instructions
-  );
+  // psiTurk.doInstructions(
+  //   flankerInstructionPages, // a list of pages you want to display in sequence
+  //   function() {
+  //     // currentview = new StroopExperiment();
+  //     currentview = new FlankerExperiment();
+  //   } // what you want to do when you are done with instructions
+  // );
+  OciRQuestionnare();
 });
